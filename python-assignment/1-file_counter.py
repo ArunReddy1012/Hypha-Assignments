4fa43919ad087def84494fdf11f69087ca6bfc4a
Arun Reddy Etta <arunreddy.e@royalcyber.com>
2024-04-28 10:57:53 +0530

    linecount 1 commit

--- python-assignment/1-file_counter.py
+++ python-assignment/1-file_counter.py
@@ -4,11 +4,11 @@
 def line_count(filename):
 #write the code for line_count function here
     try:
-        with open(filename, 'r') as file
-        readlines=file.readlines()
-        print(readlines)
-        lines=len(file.readlines())
-        return lines
+        with open(filename, 'r') as file:
+            readlines=file.readlines()
+            print(readlines)
+            lines=len(file.readlines())
+            return lines
     except Filenotfounderror:
         print("The mentioned file {filename} is missing!!!")
         sys.exit(1)
@@ -16,7 +16,7 @@ def line_count(filename):
 def main():
 #write code for main 
     filename=sys.argv[0]
-    line_count=line_count(filename)
-    print("Total no. of line in {filename} are : " + str(line_count))
+    line_count_value=line_count(filename)
+    print("Total no. of line in {filename} are : " + str(line_count_value))
 
 main()
