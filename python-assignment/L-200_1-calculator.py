1a5b5aaf00ad4fc5e779f3f24ec40c7c3f7d7d14
Arun Reddy Etta <arunreddy.e@royalcyber.com>
2024-04-28 17:00:40 +0530

    calculator 1 commit

---
+++ python-assignment/L-200_1-calculator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+#########################################################################################################
+######################## This script will perfom simple calculator operations ###########################
+#########################################################################################################
+
+
+def addition(a,b):
+    try:
+        print(a+b)
+    except ValueError:
+      print("The entered value is not a number, please make a valid entry")  
+
+def substraction(a,b):
+    try:
+        print(a-b)
+    except ValueError:
+      print("The entered value is not a number, please make a valid entry")
+
+def multiplication(a,b):
+    try:
+        print(a*b)
+    except ValueError:
+      print("The entered value is not a number, please make a valid entry")
+
+def division (a,b):
+    try:
+        print(a/b)
+    except ValueError:
+      print("The entered value is not a number, please make a valid entry")
+    except ZeroDivisionError:
+      print("You are trying to divide a number with 0, which is incorrect!!")    
+
+def switchcase(choice,a,b):
+    match choice:
+        case "1":
+            result=addition(a,b)
+        case "2":
+            result=substraction(a,b)
+        case "3":
+            result=multiplication(a,b)
+        case "4":
+            result=division(a,b)
+    
+def main():
+
+    a=float(input("Enter the value of a : "))
+    b=float(input("Enter the value of b : "))
+
+    print("Enter the number of your choice from the below operations :\n")
+    print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division")
+
+    user_input=input("")
+    switchcase(user_input,a,b)
+
+main()
