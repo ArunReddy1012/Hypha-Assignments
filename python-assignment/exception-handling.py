1aa74c60eda530259cb35736a563449044da1546
Arun Reddy Etta <arunreddy.e@royalcyber.com>
2024-04-28 14:04:02 +0530

    exception-handling 1 commit

---
+++ python-assignment/exception-handling.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/ python3
+
+
+def division_function(a,b):
+   try:
+      c=(a/b)
+      return c
+   except ValueError:
+      print("The entered value is not of number, please make a valid entry")
+    except ZeroDivisionError:
+      print("You are trying to divide a number with 0, which is incorrect!!")    
+
+def main():
+   a=float(input("Enter the value of a")) 
+   b=float(input("Enter the value of b")) 
+   result=division_function(a,b)
+   print(f"{a}/{b} is : ",result)
+
+main()
