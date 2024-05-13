a1ed272fd3d16c45325f309274b96aee621632b2
Arun Reddy Etta <arunreddy.e@royalcyber.com>
2024-05-13 12:34:59 +0530

    useraccount 1 commit

--- shellscript-assignment/13-useraccount.sh
+++ shellscript-assignment/13-useraccount.sh
@@ -6,7 +6,7 @@ echo "Audit User Account : "
 echo "==================================="
 users=$(lastlog)
 echo "List of users : "
-echo "$users" | awk -F: '{print "User : " ,$1, "         Last Login : ", $4, $5, $6, $7 }'
+echo "$users" | awk -F: '{print "User : " ,$1 }'
 
 }
 
