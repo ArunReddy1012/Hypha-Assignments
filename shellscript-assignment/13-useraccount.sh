d63ba5fa14a8f4035485bda9bcd8f9e67f10295c
Arun Reddy Etta <arunreddy.e@royalcyber.com>
2024-05-13 12:53:51 +0530

    useraccount 5 commit

--- shellscript-assignment/13-useraccount.sh
+++ shellscript-assignment/13-useraccount.sh
@@ -2,22 +2,31 @@
 
 audit_useraccounts(){
 
-echo "Audit User Account : "
-echo "==================================="
-users=$(lastlog)
-echo "List of users : "
-echo "$users" | awk -F: '{print "User : " ,$1, "Last login : " , $4 }'
+    echo "Audit User Account : "
+    echo "==================================="
+    users=$(lastlog)
+    echo "List of users : "
+    echo "$users" | awk -F: '{print "User : " ,$1 }'
 
 }
 
-#audit_filepermissi(){
-#
-#}
+audit_filepermissions(){
+    echo "Audil File Permissions : "
+    echo "===================================="
+    directories=("/var", "/etc")
+    for dir in $directories ; do
+        echo "Directory : $dir "
+        find $dir -type f -perm -o+w -exec ls -l {} +
+        echo 
+    done 
+
+
+}
 
 main(){
 
     audit_useraccounts
-    #audit_filepermissions
+    audit_filepermissions
 
 }
 
